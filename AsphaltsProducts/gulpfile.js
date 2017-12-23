14b296827754945e49bef232d7aec58438b5d10c
Harish <harishreddy1410@gmail.com>
2017-12-24 00:11:05 +0530

    added gulp watch to automate the process of less to css compiling and css minification on saving of the less file changes

--- AsphaltsProducts/gulpfile.js
+++ AsphaltsProducts/gulpfile.js
@@ -14,16 +14,32 @@ gulp.task('less', function () {
         .pipe(less())
         .pipe(gulp.dest(function (f) {
             return f.base;
-        }));
+        }))
+        .pipe(cleanCss({ compatability: 'ie8' }));
    // gulp.start('minifycss')
 });
 
-
+//Gulp task to minify the css files 
 gulp.task("minifycss", function (f) {
     return gulp.src('wwwroot/css/customcss/style.css')
-        .pipe(cleanCss({compatability:'ie8'}))
+        .pipe(cleanCss({ compatability: 'ie8' }))
         .pipe(gulp.dest(function (f) { return f.base; }))
-})
+});
+
+//Gulp task to watch out the changes to less files inside folder customcss then compiles less to CSS and minifies generated files
+gulp.watch('wwwroot/css/customcss/*.less', () => {    
+    gulp.src('wwwroot/css/customcss/style.less')
+        .pipe(less())
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }))
+        .pipe(cleanCss({ compatability: 'ie9' }))
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }));
+});
+
+
 gulp.task('default', ['less'], function () {
     gulp.watch('*.less', ['less']);
 })
