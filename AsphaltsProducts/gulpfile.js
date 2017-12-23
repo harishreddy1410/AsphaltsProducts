cc5d163f88096f6c346b31a3d73e0e54b0f18326
Harish <harishreddy1410@gmail.com>
2017-12-23 22:51:15 +0530

    1) adding less : Less is a CSS pre-processor, meaning that it extends the CSS language, adding features that allow variables, mixins, functions and many other techniques that allow you to make CSS that is more maintainable, themeable and extendable. Less runs inside Node, in the browser and inside Rhino. 3) added package.json to maintain npm packages 2) written a gulp task to compile less to css

--- AsphaltsProducts/gulpfile.js
+++ AsphaltsProducts/gulpfile.js
@@ -3,8 +3,20 @@ This file is the main entry point for defining Gulp tasks and using Gulp plugins
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
+// Less configuration
 var gulp = require('gulp');
+var less = require('gulp-less');
 
-gulp.task('default', function () {
-    // place code for your default task here
+
+//Task which compiles less to css
+gulp.task('less', function () {
+    gulp.src('wwwroot/css/customcss/style.less')
+        .pipe(less())
+        .pipe(gulp.dest(function (f) {
+            return f.base;
+        }))
 });
+
+gulp.task('default', ['less'], function () {
+    gulp.watch('*.less', ['less']);
+})
