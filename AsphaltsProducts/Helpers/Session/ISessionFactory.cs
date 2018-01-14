4446b6d57d868464c0dd6432dc36a2537eedf49e
Harish <harishreddy1410@gmail.com>
2018-01-14 16:13:03 +0530

    Configured session storage

---
+++ AsphaltsProducts/Helpers/Session/ISessionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsphaltsProducts.Presentation.Layer.Helpers.Session
+{
+    public interface ISessionFactory
+    {
+        T Get<T>(SessionKey sessionKey,Func<T> func);
+        T Get<T>(SessionKey sessionKey);
+        T Set<T>(SessionKey sessionKey,T value);
+        void Clear();
+        void RemoveSesssion(SessionKey session);
+    }
+
+    public enum SessionKey
+    {
+        SESSION_ID,
+        SESSION_OBJ
+    }
+}
